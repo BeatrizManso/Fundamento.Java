751d4000c28fe6883661fe4867efc02478d1f360
thalita.mariano <thalita.mariano@TAT0541235W10-1.senacsp.edu.br>
2019-05-29 21:20:21 -0300

    adicionando o PDV <<<

---
+++ Fundamentos/src/PDV.java
@@ -0,0 +1,15 @@
+import java.text.DateFormat;
+import java.util.Date;
+
+public class PDV {
+
+	public static void main(String[] args) {
+		//Date > trabalhar com Data
+		Date data = new Date();
+		// import java util
+		// formatar a data
+		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
+		System.out.println(formatador.format(data));
+	}
+
+}
